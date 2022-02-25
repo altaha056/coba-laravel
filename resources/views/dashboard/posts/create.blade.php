63c5cbd9c0fde50f6dd5ed054b97419c1120fe39
Altaha <altahabinsyafriadi@gmail.com>
2022-02-25 07:20:50 +0700

    add trix editor for body post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -7,15 +7,43 @@
         <form method="POST" action="/dashboard/posts">
             @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Title address</label>
+            <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" name="title">
         </div>
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-            <input type="text" class="form-control" id="slug" name="slug">
+            <input type="text" class="form-control" id="slug" name="slug" disabled readonly>
+        </div>
+        
+        <div class="mb-3">
+            <label for="category" class="form-label">Category</label>
+            <select class="form-select" name="category_id">
+                @foreach ($categories as $category)
+                    <option value={{ $category->id }}>{{ $category->name }}</option>
+                    
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="body" class="form-label">Body</label>
+            <input id="body" type="hidden" name="body">
+            <trix-editor input="body"></trix-editor>
         </div>
         
         <button type="submit" class="btn btn-primary">Create Post</button>
         </form>
     </div>
+
+    <script>
+        const title = document.querySelector('#title');
+        const slug = document.querySelector('#slug');
+
+        title.addEventListener('change', function(){
+            fetch('/dashboard/posts/createSlug?title='+title.value)
+            .then(response=>response.json())
+            .then(data=>slug.value = data.slug)
+        })
+
+        document.addEventListener('trix-file-accept', function(e){e.preventDefault();})
+    </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::get('/dashboard',function(){
     return view('dashboard.index');
 })->middleware('auth');
 
+
+Route::get('dashboard/posts/createSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 //method get    -> index
 //method post   -> store
