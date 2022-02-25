c4a459b7a1b6254dce4ca46f1e988fe08df9d072
Altaha <altahabinsyafriadi@gmail.com>
2022-02-25 19:44:59 +0700

    completing crud

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -12,8 +12,14 @@
                     </p>
                 </article>
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
-                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-                <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="return confirm('Are you sure?')">
+                    <span data-feather="x-circle"></span> Delete
+                    </button>
+                </form>
                 <article class="my-2 pt-5 pb-5 mb-5 border-bottom">                    
                     <div class="mb-5">
                         <img src="https://source.unsplash.com/1200x500?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="image-fluid">
