16cb6322a30f5468310d4e129df857f11cb1b5d4
Altaha <altahabinsyafriadi@gmail.com>
2022-03-01 19:27:55 +0700

    authorization

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardPostController;
 use App\Http\Controllers\RegisterController;
 
-Route::get('/',[HomeController::class, 'home']);
-Route::get('/home',[HomeController::class, 'home']);
-Route::get('/about',[AboutController::class, 'index']);
-Route::get('/posts',[PostController::class, 'index']);
-Route::get('/posts/{post:slug}',[PostController::class, 'show']);
-Route::get('/categories',[CategoryController::class, 'all']);
+Route::get('/', [HomeController::class, 'home']);
+Route::get('/home', [HomeController::class, 'home']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/categories', [CategoryController::class, 'all']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout',[LoginController::class, 'logout']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard',function(){
+Route::get('/dashboard', function () {
     return view('dashboard.index');
 })->middleware('auth');
 
@@ -35,3 +36,5 @@ Route::resource('/dashboard/posts', DashboardPostController::class)->middleware(
 //method post   -> store
 //method put    -> update
 //method delete -> destroy
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
